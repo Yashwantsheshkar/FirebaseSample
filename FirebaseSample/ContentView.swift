3bf39b954a45ea1d29ec9ff0b5e16e372e841633
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-08-21 21:30:26 +0530

    Initial Commit

---
+++ FirebaseSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseSample
+//
+//  Created by Yashwant Sheshkar on 21/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseSample/FirebaseSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSampleApp.swift
+//  FirebaseSample
+//
+//  Created by Yashwant Sheshkar on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
